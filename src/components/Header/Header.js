ab50b06d4d0206c8996c95b3fc8c87ce25b56beb
Shankar Kumar <shankarkumar@Shankars-MacBook-Air.local>
2021-11-19 15:29:48 +0530

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1 className="title">One Weather</h1>
+            <a className="portfolio-link" target="_new" href="_blank">Portfoilo</a>
+        </div>
+    )
+}
+
+export default Header
